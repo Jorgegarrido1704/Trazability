d5b360d7c2ab0fb775c2298b82a80ff536b86156
Jorge <garridofth@outlook.com>
2024-07-04 18:03:07 -0600

    com112t

--- app/ingTiempos.php
+++ app/ingTiempos.php
@@ -1,18 +1,12 @@
 <?php
 require "conection.php";
 require 'vendor/autoload.php'; 
-
 date_default_timezone_set("America/Mexico_City");
 $datefin=strtotime(date("d-m-Y 23:59"));
 $dateIni=strtotime(date("d-m-Y 00:00"));
-
-
-//$lider=$_POST['lider']?$_POST['lider']:"";
-//$departamento=$_POST['departamento']?$_POST['departamento']:"";
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 $spreadsheet = new Spreadsheet();$sheet = $spreadsheet->getActiveSheet();
-
 $sheet->setCellValue('A1', 'Enginner');
 $sheet->setCellValue('B1', 'Date Start');
 $sheet->setCellValue('C1', 'Date End');
@@ -21,33 +15,21 @@ $sheet->setCellValue('E1', 'Activity');
 $sheet->setCellValue('F1', 'Description');
 $t=2;
 $buscarinfo=mysqli_query($con,"SELECT * FROM ingactividades  ORDER BY Id_request DESC");
-
 while($row=mysqli_fetch_array($buscarinfo)){
     $fecha= $row['fecha'];
     $quien= $row['Id_request'];
     $fint= $row['finT'];
     $actividad= $row['actividades'];
     $desciption= $row['desciption'];
-    $tiempo=floatval(strtotime($fint)-strtotime($fecha))/60;
-
-
-   
-if($dateIni<strtotime($fecha) && $tiempo>0){
-    
-   
+    $tiempo=floatval(strtotime($fint)-strtotime($fecha))/60;   
+if($dateIni<strtotime($fecha) && $tiempo>0){   
 $sheet->setCellValue('A'.$t, $quien);
 $sheet->setCellValue('B'.$t, $fecha);
 $sheet->setCellValue('C'.$t, $fint);
 $sheet->setCellValue('D'.$t, $tiempo);
 $sheet->setCellValue('E'.$t, $actividad);
 $sheet->setCellValue('F'.$t, $desciption);
-
-
-$t++;}
-}
-
-
-    
+$t++;} }  
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
 header('Content-Disposition: attachment;filename="Engineering Activities .xlsx"');
 header('Cache-Control: max-age=0');
