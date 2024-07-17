b472640078892f22cc3803579426dc83cadc86a1
Jorge <garridofth@outlook.com>
2024-07-17 09:30:34 -0600

    updares11707

--- mantenimiento/contadorgolpes/modificar.php
+++ mantenimiento/contadorgolpes/modificar.php
@@ -5,7 +5,9 @@
 $buscar= mysqli_query($con,"SELECT fecha_reg FROM mant_golpes_diarios ORDER BY id DESC");
 while($row=mysqli_fetch_array($buscar)){
     $fecha=$row['fecha_reg'];
+    echo $fecha.'<br>';
+    
     $fecha=str_replace('-','/',$fecha);
-    echo $fecha;
+    echo $fecha.'<br>';
     
 }    
