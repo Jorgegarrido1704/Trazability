b9f39e015d12b8eb04a6870eba3bf88c2e1d1723
ingcpu <ing@cpu.com>
2024-09-12 15:10:33 -0600

    up cort

--- corte/agregarlista.php
+++ corte/agregarlista.php
@@ -1,14 +1,6 @@
 <?php
 require "../app/conection.php";
-$delCore=mysqli_query($con,"DELETE FROM corte ");
-if($delCore){
-    echo "<h1>Se Eliminaron Correctamente</h1>";
-$buscar=mysqli_query($con,"SELECT * FROM registro WHERE count<4");
-while($row=mysqli_fetch_array($buscar)){
-    $pn=$row['NumPart'];
-    $client=$row['cliente'];
-    $wo=$row['wo'];
-    $cuantos=$row['Qty'];
+$pn ="1003826612";
 $selectlist=mysqli_query($con,"SELECT * FROM listascorte WHERE pn='$pn'");
     while($rowList=mysqli_fetch_array($selectlist)){
         $cons=$rowList['cons'];
@@ -21,17 +13,16 @@ $selectlist=mysqli_query($con,"SELECT * FROM listascorte WHERE pn='$pn'");
         $dataForm=$rowList['dataFrom'];
         $dataTo=$rowList['dataTo'];
       
-       if(substr($cons,0,5)=="C"){
+       if(substr($cons,0,5)=="CORTE"){
         $cons=str_replace([".","-"," "],"",$cons);
         $codigo=substr($wo,2).substr($cons,5);
 
         }else{ $codigo=substr($wo,2).$cons;}
    
     $insertar=mysqli_query($con,"INSERT INTO corte (`np`, `cliente`, `wo`, `cons`, `color`, `tipo`, `aws`, `codigo`, `term1`, `term2`, `dataFrom`, `dataTo`, `qty`, `tamano`) VALUES ('$pn','$client','$wo','$cons','$color','$tipo','$aws','$codigo','$term1','$term2','$dataForm','$dataTo','$cuantos','$tamano')");
-}}
+}
 if($insertar){
     echo "<br><br><h1>Se Agregaron correctamente Correctamente</h1>";
     header("location:busqueda.php");
 }
 
-}
