eab13f1460802737ffed11fffba0d24caeab7adb
Jorge Garrido <garridofth@outlook.com>
2024-11-11 08:49:12 -0600

    pus

--- errores/partial.php
+++ errores/partial.php
@@ -9,6 +9,6 @@ while($row=mysqli_fetch_array($datosregistro)){
     $wo=$row['wo'];
     $info=$row['info'];
     echo $NumPart." ".$Qty." ".$wo." ".$info."<br>";
-    //$update=mysqli_query($con,"INSERT INTO registroparcial(`pn`, `wo`, `orgQty`, `codeBar`) VALUES('$NumPart','$wo','$Qty','$info')");
+    $update=mysqli_query($con,"INSERT INTO registroparcial(`pn`, `wo`, `orgQty`, `codeBar`) VALUES('$NumPart','$wo','$Qty','$info')");
 }
 
