1abd2f8836034169938846135cd642affb23f6cb
Jorge G <jgarrido@mx.bergstrominc.com>
2025-04-23 13:38:32 -0500

    ese

--- app/calidad/depuracion.php
+++ app/calidad/depuracion.php
@@ -60,3 +60,16 @@ if ($resultado) {
 
 mysqli_close($con);
 header("location:../depuracion.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="25; url=../depuracion.php">
+    <title>Calidad/Depuracion</title>
+</head>
+<body>
+    
+</body>
+</html>
--- app/depuracion.php
+++ app/depuracion.php
@@ -257,3 +257,16 @@ if ($resultado) {
 mysqli_close($con);
 header("location:../../mant/trazability/ing/piso.php");
 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="25; url=../../mant/trazability/ing/piso.php">
+    <title>Depuracion</title>
+</head>
+<body>
+    
+</body>
+</html>
