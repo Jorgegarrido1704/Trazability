a905379f2e42df5bd0f8511068a01fc54f48e6f7
Jorge G <jgarrido@mx.bergstrominc.com>
2025-08-08 10:51:28 -0500

    ese

--- ing/piso.php
+++ ing/piso.php
@@ -7,7 +7,7 @@ require "../app/conection.php";
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="refresh" content="10; url=../mantenimiento/mail.php">
+	<meta http-equiv="refresh" content="10; url=../../ing/respaldos/index.php">
         <link rel="stylesheet" href="fallas.css">
         <title>PPAP Piso</title>
     </head>
