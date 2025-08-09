e175552f5606512e55d1092c12311cd088daa7b0
Jorge G <jgarrido@mx.bergstrominc.com>
2025-08-09 09:32:44 -0500

    ese

--- log/login.php
+++ log/login.php
@@ -14,7 +14,7 @@ $users=$claves=[];
         $_SESSION['usuario'] = $username; 
         date_default_timezone_set('America/Mexico_City');
         $fecha = date('d-m-Y H:i');
-        $registro = mysqli_query($con, "INSERT INTO `registros`(`id`, `fecha`, `userName`, `action`) VALUES ('', '$fecha', '$username', '$log')");
+        $registro = mysqli_query($con, "INSERT INTO `registros`( `fecha`, `userName`, `action`) VALUES ( '$fecha', '$username', '$log')");
        header("location: ../main/principal.php");
     } else {
         header("location: ../main/index.html");
