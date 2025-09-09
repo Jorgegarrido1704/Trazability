0763cce27d5b551958028ac3ef72435b5e22079e
Jorge G <jgarrido@mx.bergstrominc.com>
2025-09-09 08:51:17 -0500

    ese

--- errores/embarque.php
+++ errores/embarque.php
@@ -1,6 +1,5 @@
 <?php
  require "../app/conection.php";
-    if(isset($_POST['registro'])){
         $count=isset($_POST['count'])?$_POST['count']:"";
     
     $registro=isset($_POST['registro'])?$_POST['registro']:"";
@@ -13,16 +12,10 @@
     //$registro=['011680','011758','011155','011387','011627','011628','011745','010998','011607','011604','011598','011599','011011','011564','011213','011067','010580','010562'];
  foreach($registro as $reg){
     $cambiarRegistro=mysqli_query($con,"UPDATE registro SET count=$count WHERE wo='$reg'");
-    if($cambiarRegistro){
-       // echo "Registro $reg cambiado correctamente al contador $count<br>";
-    }else{
-        echo "Error al cambiar el registro $reg al contador $count<br>";
-    }
+    
     
     }
        header("Location: ../corte/busqueda.php"); 
  
-    }else{
-        header("Location: ../corte/busqueda.php");
-    }
+  
 ?>
