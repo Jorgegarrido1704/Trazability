08a0feaeb044210d3b4a99be73061a4ad86d2687
Jorge G <jgarrido@mx.bergstrominc.com>
2025-12-29 07:49:17 -0600

    ese

--- bucles/altapersonal.php
+++ bucles/altapersonal.php
@@ -3,8 +3,8 @@ include "../app/conection.php";
 $week = (int)(date('W'));
 try{
 //Search fist intereaction in the table assitence for comparation
-$lastWeek=mysqli_query($con,"SELECT week FROM assistence WHERE week = $week ORDER BY id DESC LIMIT 1");
-if(mysqli_num_rows($lastWeek) <= 0){
+//$lastWeek=mysqli_query($con,"SELECT week FROM assistence WHERE week = $week ORDER BY id DESC LIMIT 1");
+//if(mysqli_num_rows($lastWeek) <= 0){
     //search all personal
     $personal = mysqli_query($con,"SELECT  employeeLider, employeeName, employeeNumber FROM personalberg WHERE `status` != 'Baja' ORDER BY `status` ASC");
     while($row = mysqli_fetch_array($personal)){
@@ -16,9 +16,9 @@ if(mysqli_num_rows($lastWeek) <= 0){
         $insert = mysqli_query($con,"INSERT INTO assistence (`week`, `lider`, `name`, `id_empleado`) VALUES ('$week', '$lider', '$name', '$idEmployee')");
     }
     
-}else{
+/*}else{
     echo "Ya se ha registrado la asistencia de esta semana";
-}
+}*/
 header("location:./assistence.php");
 }catch(Exception $e){
     echo $e->getMessage();
