600ff544000b09a6344e2faa97a9e7dadc23a135
Jorge G <jgarrido@mx.bergstrominc.com>
2026-03-27 08:59:22 -0500

    ese

--- asistencia/registrarAsistencia.php
+++ asistencia/registrarAsistencia.php
@@ -34,7 +34,13 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
         $rowRegistro = mysqli_fetch_assoc($buscarRegistro);
         if (mysqli_num_rows($buscarRegistro) <= 0) {
             $insertarAsistencia = mysqli_query($con, "INSERT INTO relogchecador (employeeNumber,fechaRegistro,entrada,comentario) VALUES ('$cardCode','$dateNow','$timeNow','$comentario')");
-            if($type=='Indirecto' and $timeNow < '08:15:00'){ 
+            if($type=='Practicante'){
+                $status='PCT';
+            }elseif($type=='Asimilado'){
+                $status='ASM';
+            }elseif($type=='Servicio comprado'){
+                $status='SCE';
+            }elseif($type=='Indirecto' and $timeNow < '08:15:00'){ 
                         $status='OK';
             }else if($type=='Indirecto'  and $timeNow > '08:15:00'){
                         $status='R';
