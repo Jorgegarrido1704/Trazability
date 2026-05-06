732c434609acd157b9460e2838871236cca8b563
Jorge G <jgarrido@mx.bergstrominc.com>
2026-05-06 11:57:55 -0500

    ese

--- fam/updateRoutingTimes.php
+++ fam/updateRoutingTimes.php
@@ -32,7 +32,7 @@ $assetsProcess = ['Cutting' => 0, 'Terminals' => 0, 'Assembly' => 0, 'Looming' =
        $valor = $procesos[$key]>0?round($procesos[$key]/60,3):0;
        $setupt = $assetsProcess[$key]>0?round($assetsProcess[$key]*1.05/60,3):0;
 
-        $valor = round($valor*1.35,2);
+        $valor = round($valor*1.05,2);
         $totalsPerProcess+=$valor; 
         //$rowText = $valor;
       //  echo "<td style='font-weight:bold;'>{$rowText}</td>";
