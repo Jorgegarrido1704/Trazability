c96be9e0ced58f0f1716538490ae3a9053834563
Jorge G <jgarrido@mx.bergstrominc.com>
2026-06-10 11:02:38 -0500

    ese

--- corte/registro.php
+++ corte/registro.php
@@ -32,8 +32,16 @@ $selectlist=mysqli_query($con,"SELECT * FROM listascorte WHERE pn='$pn' AND rev=
         $term2=$rowList['terminal2'];
         $dataForm=$rowList['dataFrom'];
         $dataTo=$rowList['dataTo'];
-       
+       $likeConsu = "Cutting cons {$cons} //%";
         $conector=$rowList['conector'];
+    $stmtTiempo = mysqli_query($con, "SELECT timePerProcess FROM routing_models WHERE pn_routing = '$pn' AND work_routing = '10001' AND work_description LIKE '$likeConsu' LIMIT 1");
+    $stmtColor  = mysqli_query($con, "SELECT tintaOrg FROM coloresencables WHERE eng_short_color = '$color' OR eng_color = '$color' OR spn_color = '$color' LIMIT 1");
+    $rowTiempo = mysqli_fetch_array($stmtTiempo);
+    $rowColor  = mysqli_fetch_array($stmtColor);
+    $tinta= $rowColor['tintaOrg'];
+    $tiempo = round(($rowTiempo['timePerProcess'] * $cuantos),2);
+    $codigo=substr($wo,2).$cons;
+
       
        if(substr($cons,0,5)=="C"){
         $cons=str_replace([".","-"," "],"",$cons);
@@ -41,7 +49,7 @@ $selectlist=mysqli_query($con,"SELECT * FROM listascorte WHERE pn='$pn' AND rev=
 
         }else{ $codigo=substr($wo,2).$cons;}
    
-    $insertar=mysqli_query($con,"INSERT INTO corte (`np`, `cliente`, `rev`, `wo`, `cons`, `color`, `tipo`, `aws`, `codigo`, `term1`, `term2`, `dataFrom`, `dataTo`, `qty`, `tamano`, `conector` ) VALUES ('$pn','$client','$rev','$wo','$cons','$color','$tipo','$aws','$codigo','$term1','$term2','$dataForm','$dataTo','$cuantos','$tamano','$conector')");
+    $insertar=mysqli_query($con,"INSERT INTO corte (`np`, `cliente`, `rev`, `wo`, `cons`, `color`, `tipo`, `aws`, `codigo`, `term1`, `term2`, `dataFrom`, `dataTo`, `qty`, `tamano`, `conector`,`tintaColor`,`time_ruteo` ) VALUES ('$pn','$client','$rev','$wo','$cons','$color','$tipo','$aws','$codigo','$term1','$term2','$dataForm','$dataTo','$cuantos','$tamano','$conector','$tinta','$tiempo')");
 }}
 
     //echo "<br><br><h1>Se Agregaron correctamente Correctamente</h1>";
---
+++ tiempos/downtimes/cuttingMachines/check_duplicate.php
@@ -0,0 +1,23 @@
+<?php
+include('./app.php');
+
+$json = file_get_contents('php://input');
+
+$requestData = json_decode($json, true);
+
+$datos = isset($requestData['data']) ? $requestData['data'] : null;
+
+$dateRegistered = $datos['dateRegistered'];
+$hourTime = $datos['hourTime'];
+$toolingCrimperName = $datos['toolingCrimperName'];
+
+$busqueda = "SELECT * FROM cutting_machine_stops WHERE fecha = '$dateRegistered'  AND maquina = '$toolingCrimperName'";
+
+$result = $conn->query($busqueda);
+
+if ($result->num_rows > 0) {
+
+    echo json_encode(array('status' => 'duplicate', 'message' => 'Duplicate entry found'));
+} else {
+    echo json_encode(array('status' => 'unique', 'message' => 'No duplicate entry found'));
+}
