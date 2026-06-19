40e8f36f657c0ccadff86bae6d03821c7779cecb
Jorge G <jgarrido@mx.bergstrominc.com>
2026-06-19 15:14:18 -0500

    ese

--- planeacion/estructuraCorte/app/pormaquina/todas.php
+++ planeacion/estructuraCorte/app/pormaquina/todas.php
@@ -55,7 +55,7 @@ try {
                FROM corte c 
                JOIN registro r ON c.wo = r.wo 
                WHERE c.cutStatus != 'Cortado' 
-               AND r.programado = 1 AND `maq_asignada` = 'MCUT-6' AND c.tamano >0 AND c.tintaColor = 'NEGRO'
+               AND r.programado = 1 AND `maq_asignada` = 'MCUT-6' AND c.tamano >0 AND c.tintaColor = 'NEGRA'
                ORDER BY c.urgencia DESC, c.aws ASC, c.term1 ASC,
                CASE WHEN c.term2 LIKE CONCAT('%',c.term1,'%') THEN 0 ELSE 1 END, c.tipo ASC";
         $maxtime = 27000;
