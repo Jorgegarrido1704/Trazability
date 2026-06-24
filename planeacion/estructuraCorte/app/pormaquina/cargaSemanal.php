3fb549e9354c61d9902dd7a89fa43d77fcaee895
Jorge G <jgarrido@mx.bergstrominc.com>
2026-06-24 15:03:24 -0500

    ese

--- planeacion/estructuraCorte/app/pormaquina/cargaSemanal.php
+++ planeacion/estructuraCorte/app/pormaquina/cargaSemanal.php
@@ -2,7 +2,7 @@
 require '../conection.php';
 
 try {
-$qry="SELECT DISTINCT r.id,r.NumPart,r.wo,r.Qty,r.programado,c.urgencia FROM corte c JOIN registro r on c.wo=r.wo WHERE `count` IN ('2','3') ORDER BY 
+$qry="SELECT DISTINCT r.id,r.NumPart,r.wo,r.Qty,r.programado,c.urgencia FROM corte c JOIN registro r on c.wo=r.wo WHERE `count` IN ('2','3','17','1') AND `cutStatus` = 'Activo' ORDER BY 
 `programado` DESC,  `urgencia` DESC, `Qty` DESC,`NumPart` DESC";
     $datos=mysqli_query($con,$qry);
     echo json_encode(mysqli_fetch_all($datos));
