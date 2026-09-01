44497aeed70295eb89b29337aad5749630f9124f
Jorge G <jgarrido@mx.bergstrominc.com>
2026-09-01 11:22:12 -0500

    ese

--- planeacion/simulacion/app/pormaquina/materialista.php
+++ planeacion/simulacion/app/pormaquina/materialista.php
@@ -3,10 +3,10 @@
 require '../conection.php';
 
 try {
-    $color = isset($_GET['color']) ? $_GET['color'] : '';
-    $awg = isset($_GET['awg']) ? $_GET['awg'] : '';
-    $tipo = isset($_GET['type']) ? $_GET['type'] : '';
-    $maquina = isset($_GET['maquina']) ? $_GET['maquina'] : 'todas';
+    $itemsSimulados = isset($_GET['items']) ? json_decode($_GET['items'], true) : [];
+    $maquinaFiltro  = isset($_GET['maquina']) ? $_GET['maquina'] : 'todas';
+
+
     $calibres = [];
     $totalCables = 0;
     $tintaNegra = 0;
@@ -21,86 +21,54 @@ try {
     // CORRECCIÓN: Inicializar la variable del acumulador de tiempo
     $tiempoTotal = 0; 
     $i = 0;
-       
+    foreach ($itemsSimulados as $item) {
+        $pn         = strtoupper(trim($item[1] ?? ''));
+        $qty        = intval($item[3] ?? 0);
     
     
-     if ($maquina == 'todas') {
-       $qry ="SELECT c.np, c.color,c.wo,c.codigo, c.aws, c.cons, c.tipo, c.tamano, c.term1, c.term2, c.tintaColor, c.qty, c.time_ruteo 
-                                             FROM corte c 
-          JOIN registro r ON c.wo = r.wo 
-                                              WHERE c.cutStatus != 'Cortado' 
-            AND r.programado = 1  AND r.count IN ('2','3','17')  AND c.tamano >0
+       $qry ="SELECT  c.color, c.aws, c.tipo, c.tamano, c.terminal1, c.terminal2
+                                             FROM listascorte c 
+        
+                                              WHERE c.tamano >0 AND c.pn = '$pn'
                                              ORDER BY
                                              c.aws ASC,
                                             c.color ASC,
                                             c.tipo ASC,
-                                             c.term1 ASC,
+                                             c.terminal1 ASC,
                                              CASE 
-                                                WHEN c.term2 LIKE CONCAT('%',c.term1,'%') THEN 0 
+                                                WHEN c.terminal2 LIKE CONCAT('%',c.terminal1,'%') THEN 0 
                                                 ELSE 1
                                             END
                                            
                                             ";
-    }else {
-     
-       $qry ="SELECT c.np, c.color,c.wo,c.codigo, c.aws, c.cons, c.tipo, c.tamano, c.term1, c.term2, c.tintaColor, c.qty, c.time_ruteo 
-                                             FROM corte c 
-          JOIN registro r ON c.wo = r.wo 
-                                              WHERE c.cutStatus != 'Cortado' 
-            AND r.programado = 1 AND `maq_asignada` = '$maquina'
-                                              AND c.tamano >0 AND r.count IN ('2','3','17')
-                                             ORDER BY  
-                                              c.aws ASC,
-                                                c.color ASC,
-                                                c.tipo ASC, 
-                                             c.term1 ASC,
-                                             CASE 
-                                                WHEN c.term2 LIKE CONCAT('%',c.term1,'%') THEN 0 
-                                                ELSE 1
-                                            END";
-                                             $maxtime=27000*3;
-    }
+
     $listasdecorte= mysqli_query($con,$qry);
     while ($rowlistas = mysqli_fetch_array($listasdecorte)) {
-        $minutos=0;
-        $pn = $rowlistas['np'];
+        
         $calibre = $rowlistas['aws'];
-        $consumo = $rowlistas['cons'];
         $tipo = $rowlistas['tipo'];
         $color = $rowlistas['color'];
-        $tamano = round($rowlistas['tamano'], 2);
-        $terminal1 = $rowlistas['term1'];
-        $terminal2 = $rowlistas['term2'];
-        $tinta = $rowlistas['tintaColor'];
-        $qty = $rowlistas['qty'];
-        //$time_ruteo = $rowlistas['time_ruteo']+300;
-        $time_ruteo = round((2.92*$qty)+180,2);
-        $minutos=round(($time_ruteo/60),2);
-        
-        // Sumamos el tiempo de la fila actual al acumulador
-        $tiempoTotal += $time_ruteo;
-        
-        // Si el tiempo acumulado NO supera los 27000, lo agregamos al array
-        if ($tiempoTotal <= $maxtime) {
+        $tamano = round($rowlistas['tamano'], 2)*$qty;
+        $terminal1 = $rowlistas['terminal1'];
+        $terminal2 = $rowlistas['terminal2'];
+
+
             // if exist the key in the array no add key else add key
             if (array_key_exists($calibre."-".$tipo."-".$color, $cables)) {
-                $cables[$calibre."-".$tipo."-".$color]+=$minutos;
+                $cables[$calibre."-".$tipo."-".$color]+=$tamano;
             }else{
-                $cables[$calibre."-".$tipo."-".$color]=$minutos;
+                $cables[$calibre."-".$tipo."-".$color]=$tamano;
             }
             if(!array_key_exists($terminal1, $terminales) && stripos($terminal1, 'Empalme') === false){
-                $terminales[$terminal1]=1;
+                $terminales[$terminal1]=$qty;
             }
             if(!array_key_exists($terminal2, $terminales) && stripos($terminal2, 'Empalme') === false ){
-                $terminales[$terminal2]=1;
+                $terminales[$terminal2]=$qty;
             }
            
-                     
-        } else {
-           
-            break;
-        }
+   
     }
+}
     $datos=array();
     $datos['cables']=$cables;
     $datos['terminales']=$terminales; 
