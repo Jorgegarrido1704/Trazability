06bb55e9252aa6696471fb900f161c2f0967ec62
Jorge G <jgarrido@mx.bergstrominc.com>
2026-09-04 13:37:50 -0500

    ese

---
+++ planeacion/checoutCorte/app/finalizar.php
@@ -0,0 +1,22 @@
+<?php
+require 'conection.php'; 
+
+try {
+    $codigo = isset($_GET['codigo']) ? $_GET['codigo'] : null;
+    if(!$codigo) {
+        throw new Exception("Código ausente.");
+    }
+    $fechaCorte = date('Y-m-d H:i:s');
+    $nuevafechaCutF= date('d-m-Y H:i');
+  
+    $changeEstatus = "UPDATE corte SET cutStatus='finalizado', fechaCorte='$fechaCorte' WHERE id='$codigo'";
+    mysqli_query($con, $changeEstatus);
+    
+    $dat['status'] = "success";
+    echo json_encode($dat);
+ 
+} catch(Exception $e) {
+    header('Content-Type: application/json');
+    echo json_encode(["status" => "error", "message" => $e->getMessage()]);
+}
+?>
