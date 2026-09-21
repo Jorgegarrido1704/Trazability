e99a44d986ed1088033733a052aaa57af5b70665
Jorge G <jgarrido@mx.bergstrominc.com>
2026-09-21 12:29:47 -0500

    ese

--- paguina/qrEtiquetas/reimpresionqr.php
+++ paguina/qrEtiquetas/reimpresionqr.php
@@ -325,6 +325,7 @@ function generarDataMatrixHTML($texto, $sizeMM = 10) {
         border: 1px solid black;
         text-align: center;
         font-size: 7px;
+        text-style: sans-serif;
        
         margin-bottom:4px;
         white-space: nowrap;
