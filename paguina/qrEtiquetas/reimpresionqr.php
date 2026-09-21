d2cc3ec44d0fbf64519617abd5a4f3fd9a58888f
Jorge G <jgarrido@mx.bergstrominc.com>
2026-09-21 11:39:30 -0500

    ese

--- paguina/qrEtiquetas/reimpresionqr.php
+++ paguina/qrEtiquetas/reimpresionqr.php
@@ -365,7 +365,7 @@ foreach ($data as $item) {
                 </div>
                 <div class="bloque2">
                     <div class="smallbox">5703</div>
-                    <div class="smallbox1"><?php echo htmlspecialchars($np."|".$rev);?></div>
+                    <div class="smallbox"><?php echo htmlspecialchars($np."|".$rev);?></div>
                     <div class="smallbox"><?php echo htmlspecialchars($today_qr."|".$consecutivoSerial); ?></div>
                 </div>
             </div>
